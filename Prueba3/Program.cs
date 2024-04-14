dee68803b9bb8b66882846ced6afc2e7b1b30917
Nicol Gustin <nicol930519@hotmail.com>
2024-04-14 11:15:49 -0500

    prueba4

---
+++ Prueba3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prueba3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Prueba4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prueba4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
